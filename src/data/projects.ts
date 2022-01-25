80585ed90b3d2d0c917ebc5070bf8c0c43bfe520
unigisper8 <fedde198212@gmail.com>
2022-01-25 08:57:46 -0500

    commit

--- src/data/projects.ts
+++ src/data/projects.ts
@@ -56,5 +56,48 @@ export default [
     title: 'Apical',
     subtitle: 'E-Commerce',
     description: 'APICAL is the digital space dedicated to operators and companies in tourism, events, and culture who want to grow and are not afraid of change. The project goal was to build the frontend of the APICAL cloud platform, which is multi-scale tour business website.'
-  }
+  },
+  {
+    categories: ['web'],
+    name: 'Faevva Pay',
+    image: '/images/projects/tcn.png',
+    website: 'https://tcn.meng7.site/',
+    tags: ['React', 'ThreeJS', 'Node', 'MongoDB'],
+    description: 'Faevva Pay is an example website built with MERN stack. TreeJS for front design and simple login & register function and has dashboard.'
+  },
+  {
+    categories: ['mobile'],
+    name: 'Havvi',
+    image: '/images/projects/havvi.png',
+    website: 'https://havvi.app',
+    tags: ['Expo', 'Firestore', 'Firebase function', 'FCM', 'NodeJS', 'Strip'],
+    apple: 'https://apps.apple.com/tt/app/havvi-fitness-home-workouts/id1577812599',
+    description: 'Havvi brings you the best home fitness content from 100+ of the world’s top creators, totaling over 16,000 workouts and growing daily.'
+  },
+  {
+    categories: ['mobile'],
+    name: 'Chill Car Service',
+    image: '/images/projects/chill.png',
+    website: 'https://chillapp.ae/',
+    tags: ['React Native', 'Firestore', 'Firebase function', 'FCM', 'Tap', 'NodeJS'],
+    description:
+      'CHILL is an online car wash platform (Interior, exterior & Disinfectant) with extended features, The only online car wash market place that connects to car wash company and customer with instant booking at the BEST price'
+  },
+  {
+    categories: ['mobile', 'web'],
+    name: 'Cloudpin',
+    image: '/images/projects/cloudpin.png',
+    website: 'https://cloudpin.it/',
+    apple: 'https://apps.apple.com/us/app/cloudpin/id1516110996',
+    tags: ['Expo', 'Firestore', 'Firebase function', 'FCM', 'Tap', 'GatsbyJS'],
+    description: 'Cloudpin lets users broadcast content to anyone in the world by simply sharing an easy to remember pin code.'
+  },
+  {
+    categories: ['web'],
+    name: 'Frame Experts',
+    image: '/images/projects/frame-experts.png',
+    website: 'https://framexperts.com/',
+    tags: ['Laravel','Vue','MySQL','Ecommerce','Stripe'],
+    description: 'Frame order website. A customer can order the frames as he wants and see how they look using builder component. Frame builder component was built with Vue.JS and admin & front area are built with Laravel..'
+  },
 ];
